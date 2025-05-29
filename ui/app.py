72f3c8d307511bdc6dcbf070fbb2ff5a0a194544
Rooi-Willie <bjviviers@gmail.com>
2025-05-29 10:26:44 +0200

    Updated dockerfile

--- ui/app.py
+++ ui/app.py
@@ -12,8 +12,7 @@ st.title("Fan Quoting App UI")
 st.write(f"Attempting to connect to API at: {API_BASE_URL}")
 
 st.write(f"Hello there!")
-st.write(f"Lekker!")
-st.write(f"Lekker2!")
+st.write(f"1222567567")
 
 try:
     response = requests.get(f"{API_BASE_URL}/")
